a074dd0fbc487795baa24721624d7bbc0d042d0d
Tony Lau <99046505+intrepidcanadian@users.noreply.github.com>
2023-10-10 18:31:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Greetings from './components/Greetings';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div> 
+        <h1>Hello World</h1>
+        <Greetings />
+
+      </div>
+
+
     </div>
   );
 }
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,13 @@
+
+function Greetings () {
+
+    return (
+
+        <h2>Hello React!</h2>
+
+    )
+
+
+}
+
+export default Greetings;
